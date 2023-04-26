f772b5f0992da8dbd84e493b711b58e9ee99af6d
Vladislav902 <holy.dev.902@gmail.com>
2023-04-27 04:36:44 +0900

    update

--- src/components/landingpage/LanguageCodeType.tsx
+++ src/components/landingpage/LanguageCodeType.tsx
@@ -1,70 +1,202 @@
-import dynamic from 'next/dynamic';
 // material-ui
-import { Box, Button, Container, Divider, Grid, Typography } from '@mui/material';
+import { Button, Container, Divider, Grid, Typography } from '@mui/material';
 
 // project imports
 import { gridSpacing } from 'store/constant';
-// vs code npm
-// import SyntaxHighlighter from 'react-syntax-highlighter';
-// // @ts-ignore
-// const { docco } = dynamic(() => import('react-syntax-highlighter/dist/esm/styles/hljs'), { ssr: false });
 
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
+
+import { useState } from 'react';
 
 // =============================|| LANDING - Language PAGE ||============================= //
-const style = dark;
+const style = a11yDark;
 
-const PreTag = ({ children }: any) => (
-    <SyntaxHighlighter language="javascript" style={style}>
-        {children}
-    </SyntaxHighlighter>
-);
 const LanguageCodeTypePage = () => {
-    // const codeString = "import { Novu } from '@novu/node';\n" +
-    //     "const novu = new Novu(process.env.NOVU_API_KEY);\n" +
-    //     "await novu.trigger('<NOTIFICATION_TEMPLATE_TRIGGER_ID>',\n" +
-    //     "{\n" +
-    //     "to: {\n" +
-    //     "subscriberId: '<UNIQUE_SUBSCRIBER_IDENTIFIER>',\n" +
-    //     "email: 'john@doemail.com',\n" +
-    //     "firstName: 'John',\n" +
-    //     "lastName: 'Doe',\n" +
-    //     "},\n" +
-    //     "payload: {\n" +
-    //     "name: 'Hello World',\n" +
-    //     "organization: {\n" +
-    //     "logo: 'https://happycorp.com/logo.png',\n" +
-    //     "},\n" +
-    //     "},\n" +
-    //     "}\n" +
-    //     ");\n";
-    const codeString = 
-    "import { Novu } from '@novu/node';\n"+
-    "const novu = new Novu(process.env.NOVU_API_KEY);\n"+
-    "await novu.trigger('<NOTIFICATION_TEMPLATE_TRIGGER_ID>',\n"+
-        "{\n"+
-            "to: {\n"+
-                "subscriberId: '<UNIQUE_SUBSCRIBER_IDENTIFIER>',\n"+
-                "email: 'john@doemail.com',\n"+
-                "firstName: 'John',\n"+
-                "lastName: 'Doe',\n"+
-            "},\n"+
-            "payload: {\n"+
-                "name: 'Hello World,'\n"+
-                "organization: {\n"+
-                    "logo: 'https://happycorp.com/logo.png',\n"+
-                "},\n"
-            "},\n"+
-        "}\n"+
-    ");";
+    const [language, setLanguage] = useState("javascript");
+    const [selectedLanguge, setSelectedLanguge] = useState(0);
+
+    const codeStrings = [
+        `import { Novu } from '@novu/node';
+const novu = new Novu(process.env.NOVU_API_KEY);
+await novu.trigger('<NOTIFICATION_TEMPLATE_TRIGGER_ID>' {
+    to: {
+        subscriberId: '<UNIQUE_SUBSCRIBER_IDENTIFIER>',
+        email: 'john@doemail.com',
+        firstName: 'John',
+        lastName: 'Doe',
+    },
+    payload: {
+        name: 'Hello World'
+        organization: {
+            logo: 'https://happycorp.com/logo.png',
+        }
+    },
+});`,
+        `require 'novu';
+
+client = Novu::Client.new('NOVU_API_KEY')
+
+body = {
+    name: "<NOTIFICATION_TEMPLATE_TRIGGER_ID>",
+    payload: {
+        name: "Hello World",
+        organization: {
+            logo: "https://happycorp.com/logo.png",
+        },
+    },
+    to: {
+        firstName: "John",
+        lastName: "Doe",
+        email: "john@doemail.com",
+        phone: "07983882199",
+    },
+}.to_json
+
+client.trigger_event(body)`,
+        `from novu.api import EventApi
+
+event_api = EventApi("https://api.novu.co/api/", "<NOVU_API_KEY>")
+event_api.trigger(
+    name="<NOTIFICATION_TEMPLATE_TRIGGER_ID>",
+    recipients="<YOUR_SUBSCRIBER_ID>",
+    payload={
+      'name': 'Hello World',
+      'organization': {
+        'logo': 'https://happycorp.com/logo.png'
+      }
+    },
+)
+  `,
+        `use Novu\SDK\Novu;
+
+$novu = new Novu(<NOVU_API_KEY>);
+
+$novu->triggerEvent([
+  'name' => '<NOTIFICATION_TEMPLATE_TRIGGER_ID>',
+  'payload' => [
+    'name' => 'Hello World',
+    'organization' => [
+			'logo': 'https://happycorp.com/logo.png',
+		],
+  ],
+  'to' => [
+      'subscriberId' => '<UNIQUE_SUBSCRIBER_IDENTIFIER>',
+      'phone' => '07983882186',
+      'email' => 'john@doemail.com',
+      'firstName' => 'John',
+      'lastName'  => 'Doe',
+  ]
+]);`,
+        `import co.novu.Novu
+import co.novu.extensions.subscribers
+import co.novu.dto.request.TriggerEventRequest
+import co.novu.dto.request.SubscriberRequest
+
+fun main() {
+    val novu = Novu(apiKey = "NOVU_API_KEY")
+
+    novu.trigger(TriggerEventRequest.Companion.invoke
+        ( 
+            name = "<NOTIFICATION_TEMPLATE_TRIGGER_ID>",
+            to = SubscriberRequest(
+                    subscriberId = "harry_potter"
+                    firstName = "Harry",
+                    lastName = "Potter",
+                    phone = "97X98XX98X1",
+                    email = "email@email.com",
+                    loacal = "locale",
+                    avatar = "avatar",
+            ),
+            payload = mapOf("name" to "Hello World")
+        )
+    )
+} 
+  `,
+    `curl -X POST
+  -H "Content-Type: application/json"
+  -H "Authorization: ApiKey REPLACE_WITH_API_KEY"
+  -d '{
+    "name": "<NOTIFICATION_TEMPLATE_TRIGGER_ID>",
+    "payload": {
+      "name": "Hello World"
+    },
+    "to": {
+      "subscriberId": "<UNIQUE_SUBSCRIBER_IDENTIFIER>",
+      "email": "john@doemail.com",
+      "firstName": "John",
+      "lastName": "Doe",
+    }
+  }' `,
+    `import (
+        "context"
+        "fmt"
+        novu "github.com/novuhq/go-novu/lib"
+        "log"
+    )
+
+subscriberID := "<UNIQUE_SUBSCRIBER_IDENTIFIER>"
+apiKey := "<NOVU_API_KEY>"
+eventId := "<NOTIFICATION_TEMPLATE_TRIGGER_ID>"
+
+ctx := context.Background()
+to := map[string]interface{}{
+    "lastName":     "Doe",
+    "firstName":    "John",
+    "subscriberId": subscriberID,
+    "email":        "john@doemail.com",
+}
+
+payload := map[string]interface{}{
+    "name": "Hello World",
+    "organization": map[string]interface{}{
+    "logo": "https://happycorp.com/logo.png",
+    },
+}
+
+data := novu.ITriggerPayloadOptions{To: to, Payload: payload}
+novuClient := novu.NewAPIClient(apiKey, &novu.Config{})
+
+resp, err := novuClient.EventApi.Trigger(ctx, eventId, data)
+if err != nil {
+    log.Fatal("novu error", err.Error())
+    return
+}
+
+fmt.Println(resp)`
+    ]
     return (
         <Container>
-            <Grid container spacing={gridSpacing} alignItems={'center'}>
-                <Grid item md={7} sm={12}>
-                    {/* <Box sx={{ minWidth: "100%", minHeight: "500px", backgroundColor: "black", borderRadius: "20px" }} > */}
-                        <PreTag>{codeString}</PreTag>
-                    {/* </Box> */}
+            <Grid container spacing={gridSpacing} alignItems={'center'} gap={"10px"}>
+                <Grid item md={6} sm={12} sx={{ backgroundColor: "black", borderRadius: "20px" }}>
+                    <Grid container gap={"5px"}>
+                        <Button variant={selectedLanguge === 0 ? 'contained' : 'outlined'} color={selectedLanguge === 0 ? "primary" : 'inherit'} sx={{ borderRadius: "20px" }} onClick={() => setSelectedLanguge(0)}>node.js</Button>
+                        <Button variant={selectedLanguge === 1 ? 'contained' : 'outlined'} color={selectedLanguge === 1 ? "primary" : 'inherit'} sx={{ borderRadius: "20px" }} onClick={() => setSelectedLanguge(1)}>Ruby</Button>
+                        <Button variant={selectedLanguge === 2 ? 'contained' : 'outlined'} color={selectedLanguge === 2 ? "primary" : 'inherit'} sx={{ borderRadius: "20px" }} onClick={() => setSelectedLanguge(2)}>Python</Button>
+                        <Button variant={selectedLanguge === 3 ? 'contained' : 'outlined'} color={selectedLanguge === 3 ? "primary" : 'inherit'} sx={{ borderRadius: "20px" }} onClick={() => setSelectedLanguge(3)}>php</Button>
+                        <Button variant={selectedLanguge === 4 ? 'contained' : 'outlined'} color={selectedLanguge === 4 ? "primary" : 'inherit'} sx={{ borderRadius: "20px" }} onClick={() => setSelectedLanguge(4)}>kotlin</Button>
+                        <Button variant={selectedLanguge === 5 ? 'contained' : 'outlined'} color={selectedLanguge === 5 ? "primary" : 'inherit'} sx={{ borderRadius: "20px" }} onClick={() => setSelectedLanguge(5)}>curl</Button>
+                        <Button variant={selectedLanguge === 6 ? 'contained' : 'outlined'} color={selectedLanguge === 6 ? "primary" : 'inherit'} sx={{ borderRadius: "20px" }} onClick={() => setSelectedLanguge(6)}>go</Button>
+                    </Grid>
+                    <Grid container sx={{
+                        '& pre': {
+                            backgroundColor: "transparent !important"
+                        }
+                    }}>
+                        <SyntaxHighlighter
+                            language={language}
+                            style={style}
+                            showLineNumbers={true}
+                            wrapLongLines={true}
+                            wrapLines={true}
+                            lineProps={lineNumber => ({
+                                style: { cursor: 'pointer' },
+                            })}
+                        >
+                            {codeStrings[selectedLanguge]}
+                        </SyntaxHighlighter>
+                    </Grid>
                 </Grid>
                 <Grid item md={5} sm={12} display={'flex'} flexDirection={'column'} gap={"20px"}>
                     <Grid item xs={12} textAlign={'left'}>
