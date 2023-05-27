5fdf4603e4efd9752cca35d0f17c91467d52560c
Sudhanshu Srivastava <sudhanshusri23@gmail.com>
2023-05-28 05:03:08 +0530

    Changed design and added Temperature setting

--- src/pages/dashboard/texts.tsx
+++ src/pages/dashboard/texts.tsx
@@ -1,20 +1,17 @@
-import { InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { InputLabel, MenuItem, Select, SelectChangeEvent, Slider, TextField } from '@mui/material';
 import { toNumber } from 'lodash';
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 
-const ChatComp = (props: { sender: string; message: string,key:number }) => {
+const ChatComp = (props: { sender: string; message: string; key: number }) => {
   return (
-    <div
-      className={`p-4 pt-1 rounded-lg`} 
-      key={props.key}
-    >
-      <p className='font-black opacity-70'>{props.sender}</p>
+    <div className={`p-4 pt-1 rounded-lg`} key={props.key}>
+      <p className="font-black opacity-70">{props.sender}</p>
       <p className="text-lg">{props.message}</p>
-      <hr/>
+      <hr />
     </div>
   );
 };
-
 
 const ChatBots: { name: string; models: string[] }[] = [
   {
@@ -32,10 +29,12 @@ const ChatBots: { name: string; models: string[] }[] = [
 ];
 
 const texts = () => {
-  const [textBox, setTextBox] = useState('Write your prompt here');
-  const [chatBot, setChatBot] = useState('');
-  const [messages, setMessages] = useState<{sender: string, message:string}[]>([])
-  const containerRef = useRef(null);
+  const [textBox, setTextBox] = useState('');
+  const [chatBot, setChatBot] = useState(ChatBots[0].name);
+  const [chatBotIndex, setChatBotIndex] = useState(0);
+  const [menuBtnActive, setMenuBtnActive] = useState(false);
+  const [temperature, setTemperature] = useState(0.7)
+  const [messages, setMessages] = useState<{ sender: string; message: string }[]>([]);
   const [currentModel, setCurrentModel] = useState(ChatBots[0].models[0]);
 
   const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -43,24 +42,36 @@ const texts = () => {
   };
 
   const chatBotSelector = (event: SelectChangeEvent) => {
-    setChatBot(event.target.value);
-    console.log(event.target.value)
+    const index = ChatBots.findIndex((item) => item.name === event.target.value)
+    setChatBotIndex(index)
+    setChatBot(ChatBots[index].name);
+    setCurrentModel(ChatBots[index].models[0])
+    console.log(event.target.value);
   };
 
   const chatModelSelector = (event: SelectChangeEvent) => {
     setCurrentModel(event.target.value);
   };
 
+  const handleMenuButton = () => {
+    setMenuBtnActive(!menuBtnActive);
+  };
+
+  function TempSlider(value:number){
+    setTemperature(value)
+    return `${value}`
+  }
+
   const handleSendMessage = async () => {
-    const newMessage = { sender: 'You', message: textBox }
+    const newMessage = { sender: 'You', message: textBox };
     setMessages((prevMessages) => [...prevMessages, newMessage]);
-    containerRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-    
+
     const data = {
-        prompt: textBox,
-        model: currentModel
+      prompt: textBox,
+      model: currentModel,
+      temperature: temperature
     };
-    setTextBox('Write your prompt here');
+    setTextBox('');
 
     const reqOptions = {
       method: 'POST',
@@ -68,7 +79,7 @@ const texts = () => {
       body: JSON.stringify(data)
     };
     let url = '';
-    switch (ChatBots[toNumber(chatBot)].name) {
+    switch (chatBot) {
       case 'Open AI':
         url = 'http://localhost:2000/api/text/openai';
         console.log(url);
@@ -82,14 +93,14 @@ const texts = () => {
         console.log(url);
         break;
       default:
-        console.log('Case not working')
+        console.log('Case not working');
         break;
     }
     console.log(url);
 
     const res = await fetch(url, reqOptions);
-    const body = await res.text()
-    const serverMess = { sender: ChatBots[toNumber(chatBot)].name, message: body }
+    const body = await res.text();
+    const serverMess = { sender: chatBot, message: body };
     setMessages((prevMessages) => [...prevMessages, serverMess]);
     console.log(serverMess);
   };
@@ -97,66 +108,98 @@ const texts = () => {
   return (
     <div className="">
       {/* Chat Bot Selector */}
-      <div className="flex justify-left bg-white rounded-lg p-4 w-full">
-        {/* Bot Selector */}
-        <div className="mx-4">
-          <InputLabel id="chatBot-select-label">Chat Engine</InputLabel>
-          <Select
-            labelId="chatBot-select-label"
-            id="chatBot-select"
-            value={chatBot}
-            label="Chat Engine"
-            onChange={chatBotSelector}
-            autoWidth
-          >
-            {/* Options of the selector  */}
-            {ChatBots.map((bot, index) => (
-              <MenuItem key={index} value={index}>
-                {bot.name}
-              </MenuItem>
-            ))}
-          </Select>
+      <div className="flex justify-between bg-white rounded-lg p-4 w-full">
+        <div className="flex">
+          {/* Bot Selector */}
+          <div className="mx-4">
+            <InputLabel id="chatBot-select-label">Chat Engine</InputLabel>
+            <Select
+              labelId="chatBot-select-label"
+              id="chatBot-select"
+              value={chatBot}
+              label="Chat Engine"
+              onChange={chatBotSelector}
+              autoWidth
+            >
+              {/* Options of the selector  */}
+              {ChatBots.map((bot, index) => (
+                <MenuItem key={index} value={bot.name}>
+                  {bot.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </div>
+
+          {/* Model Selector */}
+          <div className="mx-4">
+            <InputLabel id="chatBot-model-label">Engine Model</InputLabel>
+            <Select
+              labelId="chatBot-model-label"
+              id="chatbot-model-select"
+              value={currentModel}
+              label="Engine Model"
+              onChange={chatModelSelector}
+              autoWidth
+            >
+              {/* Options of the selector  */}
+              {ChatBots[toNumber(chatBotIndex)].models.map((model, index) => (
+                <MenuItem key={index} value={model}>
+                  {model}
+                </MenuItem>
+              ))}
+            </Select>
+          </div>
         </div>
 
-        {/* Model Selector */}
-        <div className="mx-4">
-          <InputLabel id="chatBot-model-label">Engine Model</InputLabel>
-          <Select
-            labelId="chatBot-model-label"
-            id="chatbot-model-select"
-            value={currentModel}
-            label="Engine Model"
-            onChange={chatModelSelector}
-            autoWidth
-          >
-            {/* Options of the selector  */}
-            {ChatBots[toNumber(chatBot)].models.map((model, index) => (
-              <MenuItem key={index} value={model}>
-                {model}
-              </MenuItem>
-            ))}
-          </Select>
+        {/* Advanced Menu */}
+        <div className="">
+          <button onClick={handleMenuButton} className="my-auto relative">
+            <MenuIcon sx={{ height: '28px', marginTop: 'auto', marginBottom: 'auto' }} />
+          </button>
+          <div className={`${menuBtnActive ? 'absolute right-12' : 'hidden'} bg-white p-4 border-2 border-grey rounded-sm z-[9999]`}>
+            <p className='text-lg opacity-70'>Advanced Settings</p>
+            <hr className='-mt-2'/>
+            {/* Temperature Slider */}
+            <div className='w-44'>
+              <p className=''>Temperature</p>
+              <Slider
+                aria-label="Temperature"
+                defaultValue={0.7}
+                getAriaValueText={TempSlider}
+                valueLabelDisplay="auto"
+                step={0.1}
+                marks
+                min={0.1}
+                max={2}
+              />
+            </div>
+
+              {/* Max Token Count */}
+
+          </div>
         </div>
       </div>
 
       <div className="h-full overflow-auto mb-24  grid grid-cols-1 content-end bg-white">
-        <div className='w-[40%] mx-auto'>
-        {messages.map((mess, index) => (
-          <ChatComp sender={mess.sender} message={mess.message} key={index}/>
+        <div className="w-[40%] mx-auto">
+          {messages.map((mess, index) => (
+            <ChatComp sender={mess.sender} message={mess.message} key={index} />
           ))}
-          </div>
+        </div>
       </div>
 
-      <div ref={containerRef} />
-
       <div className="fixed bottom-0 left-[20%] right-[10%] p-4 flex z-[9999]">
-        <textarea
+        <TextField
+          id="standard-multiline-flexible"
+          multiline
+          maxRows={4}
+          variant="standard"
           value={textBox}
+          placeholder='Write your prompt here'
           onChange={handleInputChange}
-          placeholder="Type your message..."
-          className="w-full text-lg pt-4 px-4 rounded-l-xl resize-y h-16 "
-        ></textarea>
-        <button onClick={handleSendMessage} className="bg-dark-blue rounded-r-xl px-4 py-2 text-white font-semibold text-lg ">
+          className='w-full text-lg pt-4 px-4 rounded-l-xl resize-y h-16 bg-white'
+        />
+        <button onClick={handleSendMessage} className="bg-dark-blue rounded-r-xl px-4 py-2 text-white font-semibold text-lg !border-0">
           Send
         </button>
       </div>
