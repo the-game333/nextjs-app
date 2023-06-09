e25af4cab813d6d9996da079b5fb8e4628c89d54
Sudhanshu Srivastava <sudhanshusri23@gmail.com>
2023-06-09 12:43:57 +0530

    Homepage textspinner fixed on mobile layout

--- src/components/landingpage/Partnership.tsx
+++ src/components/landingpage/Partnership.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Partnership = () => {
   return (
-    <div className='max-w-6xl mx-auto mb-24'>
+    <div className='md:max-w-5xl lg:max-w-6xl mx-auto mb-24'>
       <hr className='mb-16 opacity-20'/>
-        <p className='text-lg my-8 text-white opacity-60'>InfraHive works with <span className='underline'>Generative AI Companies</span></p>
-        <div className='flex justify-between mt-12 opacity-60'>
-            <img src="/assets/images/logos/openai.png" alt='Open AI Logo' className='h-12'/>
-            <span className='text-4xl font-bold text-white my-auto'>ANTHROP\C</span>
-            <img src='/assets/images/logos/cohere.svg' className='h-10'/>
-            <span className='text-4xl font-bold text-white my-auto'>stability.ai</span>
+        <p className='text-lg my-8 text-white opacity-60 mx-4'>InfraHive works with <span className='underline'>Generative AI Companies</span></p>
+        <div className='grid grid-cols-2 md:grid-cols-4  mt-12 opacity-60 items-center mx-4 gap-2'>
+            <img src="/assets/images/logos/openai.png" alt='Open AI Logo' className='h-8 md:h-12 bg-red-400 mr-8'/>
+            <span className='text-3xl md:text-4xl font-bold text-white my-auto bg-red-400 md:-ml-6'>ANTHROP\C</span>
+            <img src='/assets/images/logos/cohere.svg' className='h-8 md:h-10  bg-red-400'/>
+            <p className='text-3xl md:text-4xl font-bold text-white my-auto bg-red-400 text-center'>stability.ai</p>
         </div>
         <hr className='mt-16 opacity-20'/>
     </div>
