6dabf8652dfb9a11c48355b9fbb8269c8772364a
Hitesh Opash <hgo.opash@gmail.com>
2023-08-03 19:04:34 +0530

    fix : add 2 grib col in header sections

--- src/components/landingpage/ProductPhilosophy.tsx
+++ src/components/landingpage/ProductPhilosophy.tsx
@@ -6,83 +6,93 @@ const Partnership = () => {
   return (
     <div className="mx-auto px-3 md:max-w-5xl md:px-5 md:pb-20 md:pt-20 lg:max-w-6xl xl:px-0">
       <div id="main" className="grid grid-cols-1 justify-evenly gap-5 md:grid-cols-2 ">
-        <div className="relative rounded-3xl border border-white p-8 dark:border-[#ffffff1a]">
-          <div className="absolute right-2 top-4 flex gap-3 md:right-5 md:top-7">
-            <p className="text-white dark:text-white">In service</p>
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="38" height="20" viewBox="0 0 38 20" fill="none">
-                <rect width="38" height="20" rx="10" fill="#FEC200" />
-                <rect x="21" y="3" width="14" height="14" rx="7" fill="white" />
-              </svg>
-            </span>
+        <div>
+          <p className="mb-5 px-3 text-2xl font-medium text-white dark:text-white md:text-2xl">
+            Auto Deployed Back-End Service: Available with API reference
+          </p>
+          <div className="relative rounded-3xl border border-white p-8 dark:border-[#ffffff1a]">
+            <div className="absolute right-2 top-4 flex gap-3 md:right-5 md:top-4">
+              <p className="text-white dark:text-white">In service</p>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="38" height="20" viewBox="0 0 38 20" fill="none">
+                  <rect width="38" height="20" rx="10" fill="#FEC200" />
+                  <rect x="21" y="3" width="14" height="14" rx="7" fill="white" />
+                </svg>
+              </span>
+            </div>
+            <div className="gap-4">
+              <h6 className="text-2xl font-medium text-white dark:text-white md:text-3xl">Backend service API</h6>
+              <p className="md:text-md relative mt-5 text-lg leading-6 text-white dark:text-[#C2C2C2]">
+                Easily integrate into your application
+              </p>
+            </div>
+            <div className="mt-6 gap-4">
+              <h6 className="text-2xl font-medium text-white dark:text-white md:text-3xl">API TOKEN:</h6>
+              <p className="relative mt-3 break-all text-lg leading-6 text-[#00A3FE]  md:text-xl">https://api.infrahive.io/v1</p>
+            </div>
+            <div className="mt-2">
+              <button className="mt-5 w-full rounded-lg border p-3 text-white dark:text-white sm:w-max">API Reference</button>
+            </div>
+            <p
+              style={{
+                height: '50%',
+                width: '25%',
+                top: '30%',
+                left: '50%',
+                transform: 'translateX(50%)',
+                position: 'absolute',
+                borderRadius: '582px',
+                background: 'linear-gradient(180deg, rgba(99, 35, 196, 0.00) 0%, #FE851D 100%)',
+                filter: 'blur(100px)'
+              }}
+            ></p>
           </div>
-          <div className="gap-4">
-            <h6 className="text-2xl font-medium text-white dark:text-white md:text-3xl">Backend service API</h6>
-            <p className="relative mt-5 text-lg leading-6 text-white dark:text-[#C2C2C2] md:text-xl">
-              Easily integrate into your application
-            </p>
-          </div>
-          <div className="mt-6 gap-4">
-            <h6 className="text-2xl font-medium text-white dark:text-white md:text-3xl">API TOKEN:</h6>
-            <p className="relative mt-3 break-all text-lg leading-6 text-[#00A3FE]  md:text-xl">https://api.infrahive.io/v1</p>
-          </div>
-          <div className="mt-2">
-            <button className="mt-5 w-full rounded-lg border p-3 text-white dark:text-white sm:w-max">API Reference</button>
-          </div>
-          <p
-            style={{
-              height: '50%',
-              width: '25%',
-              top: '30%',
-              left: '50%',
-              transform: 'translateX(50%)',
-              position: 'absolute',
-              borderRadius: '582px',
-              background: 'linear-gradient(180deg, rgba(99, 35, 196, 0.00) 0%, #FE851D 100%)',
-              filter: 'blur(100px)'
-            }}
-          ></p>
         </div>
-        <div className="relative rounded-3xl border border-white p-8 dark:border-[#ffffff1a]">
-          <div className="absolute right-2 top-4 flex gap-3 md:right-5 md:top-7">
-            <p>In service</p>
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="38" height="20" viewBox="0 0 38 20" fill="none">
-                <rect width="38" height="20" rx="10" fill="#FEC200" />
-                <rect x="21" y="3" width="14" height="14" rx="7" fill="white" />
-              </svg>
-            </span>
+        <div>
+          <p className="mb-5 px-3 text-2xl font-medium text-white dark:text-white md:text-2xl">
+            Preview your App before deploying the Front-End
+          </p>
+          <div className="relative rounded-3xl border border-white p-8 dark:border-[#ffffff1a]">
+            <div className="absolute right-2 top-4 flex gap-3 md:right-5 md:top-4">
+              <p className="text-white dark:text-white">In service</p>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="38" height="20" viewBox="0 0 38 20" fill="none">
+                  <rect width="38" height="20" rx="10" fill="#FEC200" />
+                  <rect x="21" y="3" width="14" height="14" rx="7" fill="white" />
+                </svg>
+              </span>
+            </div>
+            <div className="gap-4">
+              <h6 className="text-2xl font-medium text-white dark:text-white md:text-3xl">Test-app</h6>
+              <p className="md:text-md relative mt-5 text-lg leading-6  text-white dark:text-[#C2C2C2]">Ready to use AI WebApp</p>
+            </div>
+            <div className="mt-6 gap-4">
+              <h6 className="text-2xl  font-medium text-white dark:text-white md:text-3xl">public url:</h6>
+              <p className="relative mt-3 break-all text-lg leading-6 text-[#00A3FE] md:text-xl">https://api.infrahive.io/v1</p>
+            </div>
+            <div className="mt-2 flex flex-col  gap-3 md:flex-row md:justify-center">
+              <button className="mt-5 rounded-lg border p-3 text-white dark:text-white">Preview</button>
+              <button className="mt-5 rounded-lg border p-3 text-white dark:text-white">Share</button>
+              <button className="mt-5 rounded-lg border p-3 text-white dark:text-white">Embedded</button>
+              <button className="mt-5 rounded-lg border  p-3 text-white dark:text-white">Settings</button>
+            </div>
+            <p
+              style={{
+                height: '50%',
+                width: '25%',
+                top: '30%',
+                left: '40%',
+                transform: 'translateX(50%)',
+                position: 'absolute',
+                borderRadius: '582px',
+                background: 'linear-gradient(180deg, rgba(99, 35, 196, 0.00) 0%, #FE851D 100%)',
+                filter: 'blur(100px)'
+              }}
+            ></p>
           </div>
-          <div className="gap-4">
-            <h6 className="text-2xl font-medium text-white dark:text-white md:text-3xl">Test-app</h6>
-            <p className="relative mt-5 text-lg leading-6 text-white  dark:text-[#C2C2C2] md:text-xl">Ready to use AI WebApp</p>
-          </div>
-          <div className="mt-6 gap-4">
-            <h6 className="text-2xl  font-medium text-white dark:text-white md:text-3xl">public url:</h6>
-            <p className="relative mt-3 break-all text-lg leading-6 text-[#00A3FE] md:text-xl">https://api.infrahive.io/v1</p>
-          </div>
-          <div className="mt-2 flex flex-col  gap-3 md:flex-row md:justify-start">
-            <button className="mt-5 rounded-lg border p-3 text-white dark:text-white">Preview</button>
-            <button className="mt-5 rounded-lg border p-3 text-white dark:text-white">Share</button>
-            <button className="mt-5 rounded-lg border p-3 text-white dark:text-white">Embedded</button>
-            <button className="mt-5 rounded-lg border  p-3 text-white dark:text-white">Settings</button>
-          </div>
-          <p
-            style={{
-              height: '50%',
-              width: '25%',
-              top: '30%',
-              left: '40%',
-              transform: 'translateX(50%)',
-              position: 'absolute',
-              borderRadius: '582px',
-              background: 'linear-gradient(180deg, rgba(99, 35, 196, 0.00) 0%, #FE851D 100%)',
-              filter: 'blur(100px)'
-            }}
-          ></p>
         </div>
       </div>
-      <div className="text-gray-600 dark:text-gray-300 mt-10" id="testimonials">
+      <div className="mt-10 text-gray-600 dark:text-gray-300" id="testimonials">
         <div className="mb-20 space-y-4 px-6 md:px-0">
           <h2 className="text-center text-4xl  font-bold text-white dark:text-white md:text-5xl">
             The Product{' '}
