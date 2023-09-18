fdae1b912065c85605fcde70977361f523af8e91
Vipul <007vipulrank1@gmail.com>
2023-09-18 16:03:35 +0530

    ADD: feature graphics

--- src/components/ai-agents/Features.tsx
+++ src/components/ai-agents/Features.tsx
@@ -1,24 +1,28 @@
 import clsx from 'clsx';
 import React from 'react';
+import Image from 'next/image';
 
 const DATA = [
   {
     heading: 'Natural Language Understanding',
     subHeading: 'Chatbots with Advanced Natural Language Understanding: Conversations That Feel Incredibly Natural and Easy',
     paragraph:
-      "Have you ever had a chat with a chatbot that felt surprisingly human? That's because of Natural Language Understanding (NLU). It's the special ability that allows chatbots to grasp what you're saying and respond like a real person. InfraHive's chatbots are experts at this. They understand your questions and messages effortlessly, making your conversations smooth and enjoyable.\nInfraHive's cutting-edge AI technology powers our chatbots, giving them this remarkable ability. When you engage with InfraHive's chatbots, you'll be pleasantly surprised at how easy and natural it feels, just like chatting with a helpful friend."
+      "Have you ever had a chat with a chatbot that felt surprisingly human? That's because of Natural Language Understanding (NLU). It's the special ability that allows chatbots to grasp what you're saying and respond like a real person. InfraHive's chatbots are experts at this. They understand your questions and messages effortlessly, making your conversations smooth and enjoyable.\nInfraHive's cutting-edge AI technology powers our chatbots, giving them this remarkable ability. When you engage with InfraHive's chatbots, you'll be pleasantly surprised at how easy and natural it feels, just like chatting with a helpful friend.",
+    src: '/assets/images/f-1.png'
   },
   {
     heading: 'Personalization and Context Awareness',
     subHeading: 'Chatbots That Remember Your Preferences: Tailored Recommendations and Conversations Just for You',
     paragraph:
-      "Have you ever had a chat with a chatbot that felt surprisingly human? That's because of Natural Language Understanding (NLU). It's the special ability that allows chatbots to grasp what you're saying and respond like a real person. InfraHive's chatbots are experts at this. They understand your questions and messages effortlessly, making your conversations smooth and enjoyable.\nInfraHive's cutting-edge AI technology powers our chatbots, giving them this remarkable ability. When you engage with InfraHive's chatbots, you'll be pleasantly surprised at how easy and natural it feels, just like chatting with a helpful friend."
+      "Have you ever had a chat with a chatbot that felt surprisingly human? That's because of Natural Language Understanding (NLU). It's the special ability that allows chatbots to grasp what you're saying and respond like a real person. InfraHive's chatbots are experts at this. They understand your questions and messages effortlessly, making your conversations smooth and enjoyable.\nInfraHive's cutting-edge AI technology powers our chatbots, giving them this remarkable ability. When you engage with InfraHive's chatbots, you'll be pleasantly surprised at how easy and natural it feels, just like chatting with a helpful friend.",
+    src: '/assets/images/f-2.png'
   },
   {
     heading: '24/7 Availability and Scalability',
     subHeading: "Chat Support That's Available Around the Clock: Always Ready to Assist, No Matter When You Reach Out",
     paragraph:
-      "Have you ever had a chat with a chatbot that felt surprisingly human? That's because of Natural Language Understanding (NLU). It's the special ability that allows chatbots to grasp what you're saying and respond like a real person. InfraHive's chatbots are experts at this. They understand your questions and messages effortlessly, making your conversations smooth and enjoyable.\nInfraHive's cutting-edge AI technology powers our chatbots, giving them this remarkable ability. When you engage with InfraHive's chatbots, you'll be pleasantly surprised at how easy and natural it feels, just like chatting with a helpful friend."
+      "Have you ever had a chat with a chatbot that felt surprisingly human? That's because of Natural Language Understanding (NLU). It's the special ability that allows chatbots to grasp what you're saying and respond like a real person. InfraHive's chatbots are experts at this. They understand your questions and messages effortlessly, making your conversations smooth and enjoyable.\nInfraHive's cutting-edge AI technology powers our chatbots, giving them this remarkable ability. When you engage with InfraHive's chatbots, you'll be pleasantly surprised at how easy and natural it feels, just like chatting with a helpful friend.",
+    src: '/assets/images/f-3.png'
   }
 ];
 
@@ -32,7 +36,7 @@ type P = {
 } & DataEntity;
 
 const Card = (props: P) => {
-  const { reverse, color, heading, paragraph, subHeading } = props;
+  const { reverse, color, heading, paragraph, subHeading, src } = props;
 
   const getColor = (colorArg: P['color']) => {
     let colorClass: {
@@ -73,7 +77,9 @@ const Card = (props: P) => {
 
   return (
     <div className="grid grid-cols-1 gap-6 my-12 sm:gap-12 md:grid-cols-2">
-      <div className={`${reverse ? 'md:order-1' : 'md:-order-1'} h-[300px] bg-white w-full rounded-xl`}></div>
+      <div className={`${reverse ? 'md:order-1' : 'md:-order-1'} h-[300px] bg-white w-full rounded-xl relative overflow-hidden`}>
+        <Image src={src} alt="feature" fill />
+      </div>
       <div className={`${reverse ? 'md:-order-1 md:pl-6' : 'md:order-1 md:pr-6'} self-center text-left`}>
         <div>
           <h6 className={clsx('mb-3 text-2xl', getColor(color).dark)}>{heading}</h6>
